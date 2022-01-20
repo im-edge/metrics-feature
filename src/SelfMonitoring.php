a9de4928a7cd9333fb52f08a905fd3687595c765
Thomas Gelf <thomas@gelf.net>
2022-01-20 12:39:54 +0100

    SelfMonitoring: add more counters

--- src/SelfMonitoring.php
+++ src/SelfMonitoring.php
@@ -4,70 +4,112 @@ namespace IcingaMetrics;
 
 use Evenement\EventEmitterInterface;
 use Evenement\EventEmitterTrait;
+use gipfl\IcingaPerfData\Ci;
 use gipfl\LinuxHealth\Cpu;
+use gipfl\LinuxHealth\Network;
+use gipfl\RrdTool\RrdCached\Client;
 use Psr\Log\LoggerInterface;
-use React\EventLoop\LoopInterface;
+use React\EventLoop\Loop;
+use React\Promise\ExtendedPromiseInterface;
 
 class SelfMonitoring implements EventEmitterInterface
 {
     use EventEmitterTrait;
 
-    protected $loop;
-
-    protected $redisApi;
-
-    protected $logger;
-
-    protected $ciName;
-
-    protected $fetchingRedis;
+    protected RedisPerfDataApi $redisApi;
+    protected Client $rrdCached;
+    protected LoggerInterface $logger;
+    protected ?ExtendedPromiseInterface $fetchingRedis = null;
+    protected string $ciName;
 
     public function __construct(
-        LoopInterface $loop,
         RedisPerfDataApi $redisApi,
+        Client $rrdCached,
         LoggerInterface $logger,
-        $ciName = '000-icinga-rrd-node'
+        $ciName
     ) {
-        $this->loop = $loop;
         $this->redisApi = $redisApi;
+        $this->rrdCached = $rrdCached;
         $this->logger = $logger;
         $this->ciName = $ciName;
-        $this->run();
     }
 
     protected function emitRedisCounters()
     {
         if ($this->fetchingRedis) {
+            $this->logger->notice('Redis counters are overdue');
             return;
         }
-        $this->fetchingRedis = $this->redisApi->getCounters()->then(function ($counters) {
-            $this->emit('perfData', [
-                new PerfData($this->ciName . '/RRDHealth', $counters, time())
-            ]);
-        })->always(function () {
+        $this->fetchingRedis = $this->redisApi->getCounters()->then(function ($counters = null) {
             $this->fetchingRedis = null;
+            $this->emit('perfData', [
+                new PerfData(new Ci($this->ciName, 'RRDHealth'), $this->makeCounters((array) $counters), time())
+            ]);
+        }, function (\Throwable $e) {
+            $this->fetchingRedis = null;
+            $this->logger->error($e->getMessage());
         });
+    }
+
+    protected function emitRrdCachedCounters()
+    {
+        $this->rrdCached
+            ->stats()
+            ->then(function ($result) {
+                foreach ($result as $key => & $value) {
+                    if (! in_array($key, ['QueueLength', 'TreeNodesNumber', 'TreeDepth'])) {
+                        $value .= 'c';
+                    }
+                }
+                unset($value);
+                $this->emit('perfData', [
+                    new PerfData(new Ci($this->ciName, 'RRDCacheD'), $result, time())
+                ]);
+            }, function (\Exception $e) {
+                $this->logger->error('SelfHealthCheck got no data from RRDCacheD: ' . $e->getMessage());
+            });
+    }
+
+    protected function emitInterfaceCounters()
+    {
+        foreach (Network::getInterfaceCounters() as $ifName => $counters) {
+            $this->emit('perfData', [
+                new PerfData(
+                    new Ci($this->ciName, 'Interface', $ifName),
+                    $this->makeCounters((array) $counters),
+                    time()
+                )
+            ]);
+        }
+    }
+
+    protected function makeCounters(array $counters): array
+    {
+        $result = [];
+        foreach ($counters as $key => $value) {
+            $result[$key] = $value . 'c';
+        }
+
+        return $result;
     }
 
     protected function emitCpuPerformance()
     {
         $counters = Cpu::getCounters();
-        $flat = [];
         foreach ($counters as $cpu => $cpuCounters) {
-            foreach ($cpuCounters as $label => $counter) {
-                $flat["$cpu.$label"] = $counter . 'c';
-            }
+            $this->emit('perfData', [
+                new PerfData(new Ci($this->ciName, 'CPU', $cpu), $this->makeCounters($cpuCounters), time())
+            ]);
         }
-        $this->emit('perfData', [
-            new PerfData($this->ciName . '/RRDHost', $flat, time())
-        ]);
     }
 
-    protected function run()
+    public function run($interval)
     {
-        $this->loop->addPeriodicTimer(1, function () {
+        Loop::get()->addPeriodicTimer($interval, function () {
             $this->emitRedisCounters();
+            $this->emitRrdCachedCounters();
             $this->emitCpuPerformance();
+            $this->emitInterfaceCounters();
         });
 
         $this->logger->info("SelfHealthChecker is ready to start");
