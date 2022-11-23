57b15958f2652729891e218b7460bac5ba847f9b
Thomas Gelf <thomas@gelf.net>
2022-11-23 15:40:51 +0100

    Application: central log and app name

--- bin/logger.inc.php
+++ bin/logger.inc.php
@@ -6,13 +6,14 @@ use gipfl\Log\Writer\JournaldLogger;
 use gipfl\Log\Writer\SystemdStdoutWriter;
 use gipfl\Log\Writer\WritableStreamWriter;
 use gipfl\SystemD\systemd;
+use IcingaMetrics\Application;
 use React\EventLoop\Loop;
 use React\Stream\WritableResourceStream;
 
 $logger = new Logger();
 if (systemd::startedThisProcess()) {
     if (@file_exists(JournaldLogger::JOURNALD_SOCKET)) {
-        $logger->addWriter((new JournaldLogger())->setIdentifier('icinga-metrics-shipper'));
+        $logger->addWriter((new JournaldLogger())->setIdentifier(Application::LOG_NAME));
     } else {
         $logger->addWriter(new SystemdStdoutWriter(Loop::get()));
     }
---
+++ src/Application.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace IcingaMetrics;
+
+class Application
+{
+    public const LOG_NAME = 'icinga-metrics';
+    public const PROCESS_NAME = 'IcingaMetrics';
+}
