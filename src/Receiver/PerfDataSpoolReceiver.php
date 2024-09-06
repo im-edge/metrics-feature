4fb808193010b029a4f9df0148974d4ae11443d8
Thomas Gelf <thomas@gelf.net>
2024-09-06 08:11:23 +0200

    PerfDataSpoolReceiver: formatting

--- src/Receiver/PerfDataSpoolReceiver.php
+++ src/Receiver/PerfDataSpoolReceiver.php
@@ -17,14 +17,15 @@ class PerfDataSpoolReceiver extends BaseReceiver
     {
         $redis = createRedisClient('unix://' . $this->metricStore->getRedisSocketPath());
         $redis->execute('CLIENT', 'SETNAME', ApplicationFeature::PROCESS_NAME . '::perfdataShipper');
-        $perf = new PerfDataShipper($this->logger, $this->settings->getRequired('spool-directory'));
+        $perf = new PerfDataShipper(
+            $this->logger,
+            $this->metricStore->getUuid(),
+            $this->settings->getRequired('spool-directory')
+        );
         $lua = new LuaScriptRunner($redis, dirname(__DIR__, 2) . '/lua', $this->logger);
         $perf->on(MetricsEvent::ON_MEASUREMENTS, function (array $measurements) use ($lua) {
             $result = RedisResult::toHash(
-                $lua->runScript(
-                    'shipMeasurements',
-                    array_map(JsonString::encode(...), $measurements)
-                )
+                $lua->runScript('shipMeasurements', array_map(JsonString::encode(...), $measurements))
             );
         });
         $perf->run();
